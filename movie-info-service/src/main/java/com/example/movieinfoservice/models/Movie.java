897452c9dd21c9f8084b9f5d818e80d5471071ce
Mohamed Rezq <mohamedrezq788@gmail.com>
2026-04-05 13:52:05 +0200

    add MongoDB @Document, @Id ,@field annotations to Movie model

--- movie-info-service/src/main/java/com/example/movieinfoservice/models/Movie.java
+++ movie-info-service/src/main/java/com/example/movieinfoservice/models/Movie.java
@@ -1,9 +1,16 @@
 package com.example.movieinfoservice.models;
 
-public class Movie {
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
+@Document(collection = "movies")
+public class Movie {
+    @Id
     private String movieId;
+    @Field("movie_name")
     private String name;
+    @Field("description")
     private String description;
 
     public Movie() {
