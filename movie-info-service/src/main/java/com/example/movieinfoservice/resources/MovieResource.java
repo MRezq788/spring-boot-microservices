f5d74213f0ecf9b744f1448bef00cf3949e354d4
Mohamed Rezq <mohamedrezq788@gmail.com>
2026-04-05 13:53:23 +0200

    implement MongoDB caching fallback with simulated network delay

--- movie-info-service/src/main/java/com/example/movieinfoservice/resources/MovieResource.java
+++ movie-info-service/src/main/java/com/example/movieinfoservice/resources/MovieResource.java
@@ -2,6 +2,7 @@ package com.example.movieinfoservice.resources;
 
 import com.example.movieinfoservice.models.Movie;
 import com.example.movieinfoservice.models.MovieSummary;
+import com.example.movieinfoservice.repositries.MovieRepository;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,21 +14,34 @@ import org.springframework.web.client.RestTemplate;
 @RequestMapping("/movies")
 public class MovieResource {
 
+    private final MovieRepository movieRepository;
+
     @Value("${api.key}")
     private String apiKey;
 
     private RestTemplate restTemplate;
 
-    public MovieResource(RestTemplate restTemplate) {
+    public MovieResource(RestTemplate restTemplate, MovieRepository movieRepository) {
         this.restTemplate = restTemplate;
+        this.movieRepository = movieRepository;
     }
 
     @RequestMapping("/{movieId}")
     public Movie getMovieInfo(@PathVariable("movieId") String movieId) {
-        // Get the movie info from TMDB
+        var cachedMovie = movieRepository.findById(movieId);
+        if (cachedMovie.isPresent()) {
+            return cachedMovie.get();
+        }
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+
         final String url = "https://api.themoviedb.org/3/movie/" + movieId + "?api_key=" + apiKey;
         MovieSummary movieSummary = restTemplate.getForObject(url, MovieSummary.class);
-
-        return new Movie(movieId, movieSummary.getTitle(), movieSummary.getOverview());
+        Movie fetchedMovie = new Movie(movieId, movieSummary.getTitle(), movieSummary.getOverview());
+        return movieRepository.save(fetchedMovie);
     }
 }
